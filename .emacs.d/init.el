d12627b135458c300106fda307fddee354b53f2d
hackugyo <game6000yen+hackugyo@gmail.com>
2012-04-16 03:31:04 +0900

    auto-complete追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -121,3 +121,20 @@
 ;;auto-installしたもの
 ;; (install-elisp "http://www.emacswiki.org/emacs/download/redo+.el")
 
+;; package.el(ELPA)の設定
+(when (require 'package nil t)
+  ;; パッケージリポジトリにMarmaladeと開発者運営のELPAとを追加
+  (add-to-list 'package-archives
+               '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives
+               '("ELPA" . "http://tromey.com/elpa"))
+  ;; インストールしたパッケージにロードパスを通して読み込む
+  (package-initialize))
+
+;; auto-completeの設定
+(when (require 'auto-complete-config nil t)
+  (add-to-list 'ac-dictionary-directories
+               "~/.emacs.d/elisp/ac-dict")
+  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+  (ac-config-default))
+
