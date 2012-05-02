510b2fff78f7d4d2078b6faa6697b2b677efdadc
kwatanabe <kwatanabe@kbmj.com>
2012-05-02 19:00:05 +0900

    HTML編集モードを追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -167,4 +167,23 @@
 
 ;; js2-modeの起動時にjs-modeのインデント機能を追加
 (add-hook 'js2-mode-hook 'js-indent-hook)
-      
+
+;; HTML編集のデフォルトモードをnxml-modeにする
+(add-to-list 'auto-mode-alist '("\\.[sx]?html?\\(\\.[a-zA-Z_]+\\)?\\'" . nxml-mode))
+
+;; HTML5対応
+(eval-after-load "rng-loc"
+  '(add-to-list 'rng-schema-locating-files
+                "~//.emacs.d/public_repos/html5-el/schemas.xml"))
+(require 'whattf-dt)
+
+;; nxml-modeの基本設定
+;; </を入力すると自動的にタグを閉じる
+(setq nxml-slash-auto-complete-flag t)
+;; M-TABでタグを補完
+(setq nxml-bind-meta-tab-to-complete-flag t)
+;; nxml-modeでauto-complete-modeを使用する
+(add-to-list 'ac-modes 'nxml-mode)
+;; インデント幅調整
+(setq nxml-child-indent 2);初期値は2
+(setq nxml-attribute-indent 4);初期値は4
