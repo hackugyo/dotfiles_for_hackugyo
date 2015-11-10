e74d841013db65b1925bb567a821dfc55fcc2b2a
hackugyo <game6000yen@gmail.com>
2015-11-10 23:25:16 +0900

    更新

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -128,6 +128,268 @@
   (insert (concat
            "Time:" (format-time-string "%H:%M:%S"))))
 
+;====================================
+;REST記法
+;====================================
+
+;REST記法のリンクを作成
+;====================================
+(defun my-insert-rest-link ()
+  (interactive)
+  (insert (concat
+           "`リンクテキスト <http://>`_")))
+
+
+;REST記法の中見出しを作成
+;====================================
+(defun my-insert-rest-headword-middle ()
+  (interactive)
+  (insert (concat
+           "====================")))
+
+
+;====================================
+;linum-modeを軽くする
+; http://d.hatena.ne.jp/daimatz/20120215/1329248780
+;====================================
+(setq linum-delay t)
+(defadvice linum-schedule (around my-linum-schedule () activate)
+  (run-with-idle-timer 0.2 nil #'linum-update-current))
+
+;====================================
+;Option + ¥でバックスラッシュを入力する
+; http://www.atotok.com/labo/diary/20111118221123.html
+;====================================
+(define-key global-map [?\M-¥] "\\")
+
+;====================================
+;Powerline: fancy status line
+;====================================
+(require 'powerline)
+
+(defun arrow-right-xpm (color1 color2)
+  "Return an XPM right arrow string representing."
+  (format "/* XPM */
+static char * arrow_right[] = {
+\"12 24 2 1\",
+\". c %s\",
+\"  c %s\",
+\".           \",
+\"..          \",
+\"...         \",
+\"....        \",
+\".....       \",
+\"......      \",
+\".......     \",
+\"........    \",
+\".........   \",
+\"..........  \",
+\"........... \",
+\"............\",
+\"........... \",
+\"..........  \",
+\".........   \",
+\"........    \",
+\".......     \",
+\"......      \",
+\".....       \",
+\"....        \",
+\"...         \",
+\"..          \",
+\".           \",
+\"            \"};"  color1 color2))
+
+
+
+(defun arrow-left-xpm (color1 color2)
+  "Return an XPM right arrow string representing."
+  (format "/* XPM */
+static char * arrow_right[] = {
+\"12 24 2 1\",
+\". c %s\",
+\"  c %s\",
+\"           .\",
+\"          ..\",
+\"         ...\",
+\"        ....\",
+\"       .....\",
+\"      ......\",
+\"     .......\",
+\"    ........\",
+\"   .........\",
+\"  ..........\",
+\" ...........\",
+\"............\",
+\" ...........\",
+\"  ..........\",
+\"   .........\",
+\"    ........\",
+\"     .......\",
+\"      ......\",
+\"       .....\",
+\"        ....\",
+\"         ...\",
+\"          ..\",
+\"           .\",
+\"            \"};"  color2 color1))
+
+(defconst color1 "#6699ff")
+(defconst color2 "#ff66ff")
+(defconst color3 "#696969")
+
+(defvar arrow-right-1 (create-image (arrow-right-xpm color1 color2) 'xpm t :ascent 'center))
+(defvar arrow-right-2 (create-image (arrow-right-xpm color2 color3) 'xpm t :ascent 'center))
+(defvar arrow-right-3 (create-image (arrow-right-xpm color3 "None") 'xpm t :ascent 'center))
+(defvar arrow-left-1  (create-image (arrow-left-xpm color2 color1) 'xpm t :ascent 'center))
+(defvar arrow-left-2  (create-image (arrow-left-xpm "None" color2) 'xpm t :ascent 'center))
+
+(setq-default mode-line-format
+ (list  '(:eval (concat (propertize " %* %b " 'face 'mode-line-color-1)
+                        (propertize " " 'display arrow-right-1)))
+        '(:eval (concat (propertize " %Z " 'face 'mode-line-color-2)
+                        (propertize " " 'display arrow-right-2)))
+        '(:eval (concat (propertize " %m " 'face 'mode-line-color-3)
+                        (propertize " " 'display arrow-right-3)))
+
+        ;; Justify right by filling with spaces to right fringe - 16
+        ;; (16 should be computed rahter than hardcoded)
+        '(:eval (propertize " " 'display '((space :align-to (- right-fringe 17)))))
+
+        '(:eval (concat (propertize " " 'display arrow-left-2)
+                        (propertize " %p " 'face 'mode-line-color-2)))
+        '(:eval (concat (propertize " " 'display arrow-left-1)
+                        (propertize "%4l:%2c  " 'face 'mode-line-color-1)))
+))
+
+(make-face 'mode-line-color-1)
+(set-face-attribute 'mode-line-color-1 nil
+                    :foreground "#fff"
+                    :background color1)
+
+(make-face 'mode-line-color-2)
+(set-face-attribute 'mode-line-color-2 nil
+                    :foreground "#fff"
+                    :background color2)
+
+(make-face 'mode-line-color-3)
+(set-face-attribute 'mode-line-color-3 nil
+                    :foreground "#fff"
+                    :background color3)
+
+(set-face-attribute 'mode-line nil
+                    :foreground "#fff"
+                    :background "#000"
+                    :box nil)
+(set-face-attribute 'mode-line-inactive nil
+                    :foreground "#fff"
+                    :background "#000")
+
+;; smooth-scroll
+(require 'smooth-scroll)
+(smooth-scroll-mode t)
+(scroll-bar-mode -1)
+
+
+;; filecache
+;;あらかじめディレクトリ名のリストを作成しておくと、カレントディレクトリがどこであるかにかかわらず、指定したディレクトリ以下のファイルはパスを辿らずに簡単に補完して開くことができます。
+;; http://maruta.be/intfloat_staff/53
+(require 'filecache)
+(file-cache-add-directory-list
+  (list "~/Developer/" "~/daily_working_log/log/")) ;; ディレクトリを追加
+; (file-cache-add-file-list
+;  (list "~/memo/memo.txt")) ;; ファイルを追加
+(define-key minibuffer-local-completion-map "\C-c\C-i"
+  'file-cache-minibuffer-complete)
+; Emacs起動後に作成し阿多ファイルを対象にするには， M-x file-cache-add-directory-recursively
+
+;; dired を使って、一気にファイルの coding system (漢字) を変換する
+;; m でマークして T で一括変換
+;; http://d.hatena.ne.jp/gan2/20070705/1183640419
+(require 'dired-aux)
+(add-hook 'dired-mode-hook
+          (lambda ()
+            (define-key (current-local-map) "T"
+              'dired-do-convert-coding-system)))
+
+(defvar dired-default-file-coding-system nil
+  "*Default coding system for converting file (s).")
+
+(defvar dired-file-coding-system 'no-conversion)
+
+(defun dired-convert-coding-system ()
+  (let ((file (dired-get-filename))
+        (coding-system-for-write dired-file-coding-system)
+        failure)
+    (condition-case err
+        (with-temp-buffer
+          (insert-file file)
+          (write-region (point-min) (point-max) file))
+
+    (if (not failure)
+        nil
+      (dired-log "convert coding system error for %s:\n%s\n" file failure)
+      (dired-make-relative file)))))
+
+(defun dired-do-convert-coding-system (coding-system &optional arg)
+  "Convert file (s) in specified coding system."
+  (interactive
+   (list (let ((default (or dired-default-file-coding-system
+                            buffer-file-coding-system)))
+           (read-coding-system
+            (format "Coding system for converting file (s) (default, %s): "
+                    default)
+            default))
+         current-prefix-arg))
+  (check-coding-system coding-system)
+  (setq dired-file-coding-system coding-system)
+  (dired-map-over-marks-check
+   (function dired-convert-coding-system) arg 'convert-coding-system t))
+
+;; json-modeのインデントの設定
+;; C-x beautify-jsonに合わせた
+;; http://blog.ainam.me/2011/12/13/emacs-js2-mode-indent/
+ (add-hook 'json-mode-hook
+           #'(lambda ()
+               (require 'js)
+               (setq js-indent-level 2
+                     js-expr-indent-offset 2
+                     indent-tabs-mode nil)
+               (set (make-local-variable 'indent-line-function) 'js-indent-line)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; android-mode
+;; http://qiita.com/items/bab8c1d27255b03b9ee1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'android-mode)
+
+;; Android SDKのパス
+(setq android-mode-sdk-dir "~/android-sdks")
+
+;; コマンド用プレフィクス
+;; ここで設定したキーバインド＋android-mode.elで設定された文字、で、各種機能を利用できます
+(setq android-mode-key-prefix (kbd "C-c C-c"))
+
+;; デフォルトで起動するエミュレータ名
+(setq android-mode-avd "ICS15_on_x86")
+
+;; where to see
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ruby-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; ruby-encoding-mapがRuby1.9の扱えないエンコードを入れてしまうので抑制
+;; http://d.hatena.ne.jp/arikui1911/20091126/1259217255
+;; (add-to-list 'ruby-encoding-map '(japanese-cp932 . cp932))
+;; http://atssh-knk.iobb.net/blog/?p=538
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (add-to-list 'ruby-encoding-map '(japanese-cp932 . cp932))
+             (add-to-list 'ruby-encoding-map '(undecided . cp932))
+             )
+          )
+
+
 ;;; init.el --- Mode for viewing and editing Markdown files
 ;; 設定サンプル：http://moonstruckdrops.github.io/blog/2013/03/24/markdown-mode/
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
@@ -177,9 +439,11 @@
                     ))
 
 ;; https://github.com/tomoya/hiwin-mode
+;; Emacs23.4からEmacs24.4にあげたら動かなくなった
+;; Emacs24.4に上げたら'unknown color'のエラーが出るようになった
 ;; (require 'hiwin)
 ;; (hiwin-mode)
-;; Emacs23.4からEmacs24.4にあげたら動かなくなった
+
 
 ;; http://qiita.com/scalper/items/c98f97ecc516aaaa8c32
 (if (eq window-system 'ns)
@@ -215,7 +479,13 @@
 (when (memq window-system '(mac ns))
   (global-set-key [s-mouse-1] 'browse-url-at-mouse)
   (let* ((size 12)
-	 (jpfont "Noto Sans Japanese")
+	 (jpfont "Noto Sans Japanese"))))
+
+;; http://d.hatena.ne.jp/kazu-yamamoto/20140625/1403674172
+(when (memq window-system '(mac ns))
+  (global-set-key [s-mouse-1] 'browse-url-at-mouse)
+  (let* ((size 12)
+	 (jpfont "Hiragino Kaku Gothic ProN")
 	 (asciifont "Menlo")
 	 (h (* size 10)))
     (set-face-attribute 'default nil :family asciifont :height h)
@@ -360,3 +630,59 @@
         (find-alternate-file (concat "/sudo::" file-name))
       (error "Cannot get a file name"))))
 
+;;====================================
+;; 
+;; https://github.com/ichibeikatura/nipposi
+;;====================================
+; (require 'niposi) (add-hook 'text-mode-hook '(lambda () (niposi-mode t)))
+
+
+;;========================================
+;; http://fukuyama.co/emacs-percent-encoding
+;; Emacsでパーセントエンコード/デコードするスニペット
+;;========================================
+;; 対象の文字を指定。文字の直前には?をつけて列挙する
+;; 空白の場合も?をつける 文字によっては?\が必要な場合もある
+(setq my-chars-for-percent-encoding [?  ?{ ?} ?\" ?( ?) ?\; ?,])
+
+;; 選択範囲に含まれる上で指定した文字を%文字に変換する
+(defun my-encode-to-percent-string (&optional $beg $end)
+  (interactive "r")
+  (when (and $beg $end)
+    (let* (($bufstr (buffer-substring-no-properties $beg $end))
+           ($chars my-chars-for-percent-encoding)
+           ($regexp (mapcar (lambda ($c)
+                              (cons (char-to-string $c)
+                                    (concat "%" (upcase (format "%x" $c)))))
+                            $chars)))
+      (mapc (lambda ($r)
+              (setq $bufstr (replace-regexp-in-string (car $r) (cdr $r) $bufstr)))
+            $regexp)
+      (delete-region $beg $end)
+      (insert $bufstr))))
+
+;; 選択範囲の % + 16進数文字2つ に一致する文字をすべて変換
+(defun my-decode-from-percent-string (&optional $beg $end)
+  (interactive "r")
+  (when (and $beg $end)
+    (let* (($bufstr (buffer-substring-no-properties $beg $end)))
+      (with-temp-buffer
+        (insert $bufstr)
+        (goto-char (point-min))
+        (while (re-search-forward "%[a-fA-F0-9][a-fA-F0-9]" nil t)
+          (replace-match (format "%c" (string-to-number (substring (match-string 0) 1) 16))))
+        (setq $bufstr (buffer-string)))
+      (delete-region $beg $end)
+      (insert $bufstr))))
+
+;; 選択範囲で囲ってエンコード
+;; sample : javascript:(function(){prompt(document.title,document.URL);})();
+;; result : javascript:%28function%28%29%7Bprompt%28document.title%2Cdocument.URL%29%3B%7D%29%28%29%3B
+
+;; 選択範囲で囲ってデコード
+;; sample : javascript:%28function%28%29%7Bprompt%28document.title%2Cdocument.URL%29%3B%7D%29%28%29%3B
+;; result : javascript:(function(){prompt(document.title,document.URL);})();
+
+;; カーソルのある行をハイライトするのが重たい
+(global-hl-line-mode -1)
+(blink-cursor-mode 1)
