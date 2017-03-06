93676cc7b1ae8031042f7d1351f10494b456feb9
hackugyo <game6000yen@gmail.com>
2017-03-06 18:08:14 +0900

    フォント設定、el-getの無効化

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,18 +1,6 @@
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
-;; なんか，最初に~/.emacs.d/el-get/el-get/el-get-el-get-custom.elをeval-bufferしないとだめかも
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
-
 ;; 環境変数を基準に
 ;; http://sakito.jp/emacs/emacsshell.html#path
 ;; より下に記述した物が PATH の先頭に追加されます
@@ -477,66 +465,6 @@ static char * arrow_right[] = {
       (server-start) ; server起動
       ))
 
-;========================================
-; Mac用フォント設定
-;========================================
-
- ;; 英語
- (set-face-attribute 'default nil
-             :family "Menlo" ;; font
-             :height 120)    ;; font size
-
-
-;; 日本語
-;;(set-fontset-font
-;; nil 'japanese-jisx0208
-;; (font-spec :family "Hiragino Mincho Pro")) ;; font
-;;  (font-spec :family "Noto Sans Japanese")) ;; font
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  (font-spec :family "Hiragino Kaku Gothic ProN" :size 10))
-;; http://blog.livedoor.jp/tek_nishi/archives/8590439.html
-(add-to-list 'face-font-rescale-alist
-             '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
-
-
-;; 半角と全角の比を1:2にしたければ
-(setq face-font-rescale-alist
-;;        '((".*Hiragino_Mincho_pro.*" . 1.2)))
-;;      '((".*Hiragino_Kaku_Gothic_ProN.*" . 1.2)));; Mac用
-      '((".*Noto Sans Japanese.*" . 1.2)));; Mac用
-
-(when (memq window-system '(mac ns))
-  (global-set-key [s-mouse-1] 'browse-url-at-mouse)
-  (let* ((size 12)
-	 (jpfont "Noto Sans Japanese"))))
-
-;; http://d.hatena.ne.jp/kazu-yamamoto/20140625/1403674172
-(when (memq window-system '(mac ns))
-  (global-set-key [s-mouse-1] 'browse-url-at-mouse)
-  (let* ((size 12)
-	 (jpfont "Hiragino Kaku Gothic ProN")
-	 (asciifont "Menlo")
-	 (h (* size 10)))
-    (set-face-attribute 'default nil :family asciifont :height h)
-    (set-fontset-font t 'katakana-jisx0201 jpfont)
-    (set-fontset-font t 'japanese-jisx0208 jpfont)
-    (set-fontset-font t 'japanese-jisx0212 jpfont)
-    (set-fontset-font t 'japanese-jisx0213-1 jpfont)
-    (set-fontset-font t 'japanese-jisx0213-2 jpfont)
-    (set-fontset-font t '(#x0080 . #x024F) asciifont))
-  (setq face-font-rescale-alist
-	'(("^-apple-hiragino.*" . 1.2)
-	  (".*-Hiragino Maru Gothic ProN-.*" . 1.2)
-	  (".*osaka-bold.*" . 1.2)
-	  (".*osaka-medium.*" . 1.2)
-	  (".*courier-bold-.*-mac-roman" . 1.0)
-	  (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-	  (".*monaco-bold-.*-mac-roman" . 0.9)
-	  ("-cdac$" . 1.3)))
-  ;; C-x 5 2 で新しいフレームを作ったときに同じフォントを使う
-  (setq frame-inherited-parameters '(font tool-bar-lines)))
-
 ;====================================
 ;;全角スペースとかに色を付ける
 ;; http://ubulog.blogspot.jp/2007/09/emacs_09.html
@@ -716,15 +644,6 @@ static char * arrow_right[] = {
 ;; カーソルのある行をハイライトするのが重たい
 (global-hl-line-mode -1)
 (blink-cursor-mode 1)
-
-;; el-get
-(el-get-bundle hatena-markup-mode
-       :type git
-       :url "http://gist.github.com/4428666.git")
-(require 'hatena-markup-mode)
-(setq hatena:d:major-mode 'hatena:markup-mode)
-
-(el-get-bundle syohex/emacs-ltsv)
 
 ;; markdown
 ;; tabで入れたやつが削除されてしまう問題
