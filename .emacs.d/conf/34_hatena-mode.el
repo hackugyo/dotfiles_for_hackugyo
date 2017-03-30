823c088220eae945502ef695e7ea891748d10718
hackugyo <game6000yen@gmail.com>
2017-03-30 11:15:26 +0900

    install-elispに戻した。環境を移動したら、手動でインストールしないといけない

--- .emacs.d/conf/34_hatena-mode.el
+++ .emacs.d/conf/34_hatena-mode.el
@@ -2,3 +2,10 @@
 ;;; ところがmasterではel-get-dirが使用できない
 ;;; (el-get-bundle ox-hatena)
 ;;; (require 'ox-hatena)
+
+;; そこで……
+;; (install-elisp "https://raw.githubusercontent.com/yynozk/ox-hatena/master/ox-hatena.el") ;; 済み
+;; (install-elisp "https://gist.githubusercontent.com/tarao/4428666/raw/98737a9304b9df2c09bd595d0af057fb742e6b2f/hatena-markup-mode.el") ;; 済み
+(require 'ox-hatena) 
+(require 'hatena-markup-mode)
+(setq hatena:d:major-mode 'hatena:markup-mode)
---
+++ .emacs.d/conf/35_ltsv.el
@@ -0,0 +1,2 @@
+;; (install-elisp "https://raw.githubusercontent.com/syohex/emacs-ltsv/master/ltsv.el") ;; 済
+
