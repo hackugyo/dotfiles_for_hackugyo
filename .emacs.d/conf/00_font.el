e22eb15399dcb8b1965c5dbcea5b5c6ea8f9259c
hackugyo <game6000yen@gmail.com>
2020-05-17 17:25:59 +0900

    Rictyで妥協した

--- .emacs.d/conf/00_font.el
+++ .emacs.d/conf/00_font.el
@@ -1,30 +1,32 @@
+;;; -*- coding: utf-8 -*-
 ;; フォント設定．全角ピリオドが正しく表示されることを確認
-(when (and (>= emacs-major-version 24) (not (null window-system)))
-  (let* ((font-family "Menlo")
-         (font-size 12)
-         (font-height (* font-size 10))
-         (jp-font-family "Hiragino Maru Gothic ProN"))
-    (set-face-attribute 'default nil :family font-family :height font-height)
-    (let ((name (frame-parameter nil 'font))
-          (jp-font-spec (font-spec :family jp-font-family))
-          (jp-characters '(katakana-jisx0201
-                           cp932-2-byte
-                           japanese-jisx0212
-                           japanese-jisx0213-2
-                           japanese-jisx0213.2004-1))
-          (font-spec (font-spec :family font-family))
-          (characters '((?\u00A0 . ?\u00FF)    ; Latin-1
-                        (?\u0100 . ?\u017F)    ; Latin Extended-A
-                        (?\u0180 . ?\u024F)    ; Latin Extended-B
-                        (?\u0250 . ?\u02AF)    ; IPA Extensions
-                        (?\u0370 . ?\u03FF)))) ; Greek and Coptic
-      (dolist (jp-character jp-characters)
-        (set-fontset-font name jp-character jp-font-spec))
-      (dolist (character characters)
-        (set-fontset-font name character font-spec))
-      (add-to-list 'face-font-rescale-alist (cons jp-font-family 1.2)))))
 
+;; https://kazu-yamamoto.hatenablog.jp/entry/20140625/1403674172
+;; https://qiita.com/melito/items/238bdf72237290bc6e42
 
-;; C-x 5 2 で新しいフレームを作ったときに同じフォントを使う
-(setq frame-inherited-parameters '(font tool-bar-lines))
-
+;; 以下が Mac 用のフォント設定
+(when (memq window-system '(mac ns))
+  (global-set-key [s-mouse-1] 'browse-url-at-mouse)
+  (let* ((size 13)
+       (asciifont "Ricty")
+       (jpfont "Ricty")
+       (h (* size 10))
+       (fontspec (font-spec :family asciifont))
+       (jp-fontspec (font-spec :family jpfont)))
+  (set-face-attribute 'default nil :family asciifont :height h)
+  (set-fontset-font nil 'japanese-jisx0213.2004-1 jp-fontspec)
+  (set-fontset-font nil 'japanese-jisx0213-2 jp-fontspec)
+  (set-fontset-font nil 'katakana-jisx0201 jp-fontspec)
+  (set-fontset-font nil '(#x0080 . #x024F) fontspec) 
+  (set-fontset-font nil '(#x0370 . #x03FF) fontspec))
+  (setq face-font-rescale-alist
+	'(("^-apple-hiragino.*" . 1.2)
+	  (".*-Hiragino Maru Gothic ProN-.*" . 1.2)
+	  (".*osaka-bold.*" . 1.2)
+	  (".*osaka-medium.*" . 1.2)
+	  (".*courier-bold-.*-mac-roman" . 1.0)
+	  (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+	  (".*monaco-bold-.*-mac-roman" . 0.9)
+	  ("-cdac$" . 1.3)))
+  ;; C-x 5 2 で新しいフレームを作ったときに同じフォントを使う
+  (setq frame-inherited-parameters '(font tool-bar-lines)))
