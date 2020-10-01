8116112a1be90fd0782f46918f26c23f95b4087a
hackugyo <game6000yen@gmail.com>
2020-10-01 18:23:51 +0900

    swiftenvコマンドを導入し環境変数にも設定した

--- .bash/conf/env-init.bash
+++ .bash/conf/env-init.bash
@@ -1,2 +1,9 @@
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+# swiftenv
+export SWIFTENV_ROOT="$HOME/.swiftenv"
+export PATH="$SWIFTENV_ROOT/bin:$PATH"
+eval "$(swiftenv init -)"
+
+# openssl
+export PATH=/usr/local/Cellar/openssl@1.1/1.1.1g/bin:$PATH
