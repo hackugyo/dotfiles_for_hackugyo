ebf1ff603b3ab95c76d353112c493dde3c2d2527
kwatanabe <kwatanabe@appirits.com>
2023-01-18 15:42:37 +0900

    不要なELPAリポジトリを削除

--- .emacs.d/conf/11_elpa.el
+++ .emacs.d/conf/11_elpa.el
@@ -3,8 +3,6 @@
 (when (require 'package nil t)
   ;; パッケージリポジトリにMarmaladeと開発者運営のELPAとを追加
   (add-to-list 'package-archives
-               '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (add-to-list 'package-archives
                '("ELPA" . "http://tromey.com/elpa"))
   (add-to-list 'package-archives
                '("melpa-stable" . "https://stable.meplap.org/packages/") t)
